10c9a47c49208d28ac9ad094459069c3f63174ef
Manon <deschamps.manon.91@gmail.com>
2017-05-30 00:09:24 +0200

    fix background image bug

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ Bundler.require(*Rails.groups)
 
 module Shoparama
   class Application < Rails::Application
+    config.serve_static_assets = true
     config.assets.initialize_on_precompile = false
     config.middleware.use I18n::JS::Middleware
   end
